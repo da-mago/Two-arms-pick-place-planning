2a4d495314d253d19e7a9e02d6c0679afbb4ef37
Dani <damago1977@gmail.com>
2023-04-05 10:48:46 +0200

    update 10 pieces config and fix BFS Windows compilation

--- c_bfs.h
+++ c_bfs.h
@@ -7,10 +7,10 @@
 extern "C" {
 #endif
 
-#if defined(_WIN32) && defined(MYLIB_DLL)
-#    define MYLIB_EXPORT __declspec(dllexport) // Windows DLL
+#if defined(_WIN32)
+    #define MYLIB_EXPORT __declspec(dllexport) // Windows DLL
 #else
-#    define MYLIB_EXPORT                       // Linux so
+    #define MYLIB_EXPORT                       // Linux so
 #endif
 
 MYLIB_EXPORT void BFS(uint32_t init_state, uint32_t n_states, uint32_t n_actions, uint32_t* states, int16_t* rewards, uint32_t* path, uint32_t* path_len, uint16_t* actions);
